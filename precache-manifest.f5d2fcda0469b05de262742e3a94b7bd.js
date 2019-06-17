d587b2de25830a460e2b9cc0f321074f42d0f28b
Rphmelo <raphael_soft@hotmail.com>
2019-06-16 22:40:43 -0300

    Updates

---
+++ precache-manifest.f5d2fcda0469b05de262742e3a94b7bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8570e7aaf003823fe8f8bc6d2f1d5db",
+    "url": "/react-fiap/index.html"
+  },
+  {
+    "revision": "391749d297d944255a62",
+    "url": "/react-fiap/static/css/main.5691e588.chunk.css"
+  },
+  {
+    "revision": "75433550db4e5ccfa8e3",
+    "url": "/react-fiap/static/js/2.99f501d6.chunk.js"
+  },
+  {
+    "revision": "391749d297d944255a62",
+    "url": "/react-fiap/static/js/main.9d77ac9d.chunk.js"
+  },
+  {
+    "revision": "e3d4fb7c47708b57360d",
+    "url": "/react-fiap/static/js/runtime~main.3a489658.js"
+  },
+  {
+    "revision": "913d00d949a6cc03279b5cab517c75f6",
+    "url": "/react-fiap/static/media/react_shop_app_logo.913d00d9.png"
+  }
+]);
